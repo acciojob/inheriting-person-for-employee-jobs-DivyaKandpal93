f902190b97e26ef8820aafae06f351454862a0aa
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-02-18 20:54:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-class Person{
-    constructor(name, age){
+class Person {
+    constructor(name, age) {
         this._name = name;
         this._age = age;
     }
 
-    greet(){
-        return console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
+    greet() {
+        console.log(`Hello, my name is ${this._name} and I am ${this._age} years old.`);
     }
 }
 
-class Employee extends Person{
-    constructor(name, age, jobTitle){
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
         super(name, age);
         this._jobTitle = jobTitle;
     }
     
-    jobGreet(){
-        return console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
+    jobGreet() {
+        console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
     }
 }
 
 const alice = new Person("Alice", 25);
-alice.greet(); // This should log 'Hello, my name is Alice and I am 25 years old.'
+alice.greet(); 
 
 const bob = new Employee("Bob", 30, "Manager");
-bob.jobGreet(); // This should log 'Hello, my name is Bob, I am 30 years old, and my job title is Manager.'
+bob.jobGreet(); 
 
 // Do not change code below this line
 window.Person = Person;
