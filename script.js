d1ee4d9852cf5e1f06e687ca097158944912c902
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-02-18 20:49:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ class Person{
         this._age = age;
     }
 
-greet(){
-    return console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
+    greet(){
+        return console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
+    }
 }
-}
+
 class Employee extends Person{
     constructor(name, age, jobTitle){
-       
         super(name, age);
-         this._jobTitle = jobTitle;
-         
+        this._jobTitle = jobTitle;
     }
     
     jobGreet(){
         return console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
     }
 }
+
 const obj2 = new Employee("Vivek",32,"manager");
 obj2.jobGreet();
 obj2.greet();
